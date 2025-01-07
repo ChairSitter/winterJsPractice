4e33ae895759e36a60c3b8610a19b8d1aaa742ec
Bryan Schneller <bryan.schneller@gmail.com>
2025-01-07 14:42:51 -0500

    Add monkeyTrouble problem

--- warmup.js
+++ warmup.js
@@ -2,9 +2,20 @@ const sleepIn = (weekday, vacation) => {
     return !weekday || vacation;
 }
 
-console.log('sleepIn results:')
+console.log('sleepIn results:');
 console.log(sleepIn(true, true)); //expect true
 console.log(sleepIn(true, false)); //expect false
 console.log(sleepIn(false, true)); //expect true
 console.log(sleepIn(false, false)); //expect true
+
+const monkeyTrouble = (aSmile, bSmile) => {
+    return (aSmile === bSmile);
+}
+
+console.log('monkeyTrouble results:');
+console.log(monkeyTrouble(true, true)); //expect true
+console.log(monkeyTrouble(true, false)); //expect false
+console.log(monkeyTrouble(false, true)); //expect false
+console.log(monkeyTrouble(false, false)); //expect 
+
 
